7755b21bbd1a8eb8edc1d6f9c03efe59d4099538
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2025-12-26 08:30:04 +0700

    initial : commit, preparing Next.js 16 and Selia component
    Selia by https://github.com/nauvalazhar

---
+++ globals.d.ts
@@ -0,0 +1,21 @@
+/**
+ * declarations for importing various asset types in TypeScript projects.
+ * These declarations allow TypeScript to understand imports of non-code files,
+ * enabling type checking and module resolution for assets like images, stylesheets, and fonts.
+ * 4yasenime, superti4r
+ */
+
+declare module '*.css';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.gif';
+declare module '*.webp';
+declare module '*.mp4';
+declare module '*.mp3';
+declare module '*.woff';
+declare module '*.woff2';
+declare module '*.ttf';
+declare module '*.eot';
+declare module '*.otf';
